b66097e9fe145e5215dd3da1c973a6ad4d3adb76
filipperogerio <filipperogarioa@gmail.com>
2021-06-03 19:53:01 -0300

    hook customizado para tratamento de erros

--- src/components/FormularioCadastro/DadosPessoais.jsx
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,41 +1,22 @@
 import React, { useState, useContext } from 'react'
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core'
 import ValidacoesCadastro from '../../contexts/ValidacoesCadastroContext'
+import useErros from '../../hooks/useErros'
 
 function DadosPessoais ({ aoEnviar }) {
+  const validacoes = useContext(ValidacoesCadastro)
   const [nome, setNome] = useState('')
   const [sobrenome, setSobrenome] = useState('')
   const [cpf, setCpf] = useState('')
   const [promocoes, setPromocoes] = useState(false)
   const [novidades, setNovidades] = useState(false)
-  const [erros, setErros] = useState({
-    cpf: { valido: true, texto: '' },
-    nome: { valido: true, texto: '' }
-  })
-
-  const validacoes = useContext(ValidacoesCadastro)
+  const [erros, validarCampos, possoEnviar] = useErros(validacoes)
 
   const handleForm = e => {
     e.preventDefault()
     if (possoEnviar()) {
       aoEnviar({ nome, sobrenome, cpf, promocoes, novidades })
     }
-  }
-
-  const possoEnviar = () => {
-    for (let campo in erros) {
-      if (!erros[campo].valido) {
-        return false
-      }
-    }
-    return true
-  }
-
-  const validarCampos = event => {
-    const { name, value } = event.target
-    const novoEstado = { ...erros }
-    novoEstado[name] = validacoes[name](value)
-    setErros(novoEstado)
   }
 
   return (
