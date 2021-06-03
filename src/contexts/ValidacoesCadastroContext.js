dec640196d549f39dc7cfd791095b8b6ecb4ead7
filipperogerio <filipperogarioa@gmail.com>
2021-06-03 19:39:16 -0300

    validações padroões, código apenas de exemplo

--- src/contexts/ValidacoesCadastroContext.js
+++ src/contexts/ValidacoesCadastroContext.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { validarCpf, validarSenha, validarNome } from '../models/cadastro'
 
-const ValidacoesCadastro = React.createContext()
+const ValidacoesCadastro = React.createContext({
+  //   cpf: validarCpf,
+  //   nome: validarNome,
+  //   senha: validarSenha
+})
+
+function retornoDefault () {
+  return { valido: true, texto: '' }
+}
 
 export default ValidacoesCadastro
